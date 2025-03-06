14b737645f3ee937cc33b4e06ef7400b4646e134
plnagaraju <plnr206@gmail.com>
2025-03-06 11:15:25 +0530

    Arrays default values demo

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,30 @@ public class Main {
 
         for (int j : arr) {
 
-            System.out.println(j);
+            System.out.print(j);
 
         }
 
+        defaultValuesDemo();
 
+
+    }
+
+    private static void defaultValuesDemo() {
+        System.out.println(" array default values demo ");
+
+        // int array
+        System.out.println("\n int array default values demo \n");
+        int[] num = new int[5];
+        for(int i:num) {
+            System.out.print(i + " ");
+        }
+
+        // string array
+        System.out.println("\n string array default values demo \n");
+        String[] arr = new String[5];
+        for(String j : arr) {
+            System.out.print(j + " ");
+        }
     }
 }

91c5817fc6265fd07117b156ff355d236af928be
plnagaraju <plnr206@gmail.com>
2025-03-06 10:46:00 +0530

    Basics of Arrays

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        int[] arr = {1,2,3,4};
+
+        int size = arr.length;
+
+        for (int j : arr) {
+
+            System.out.println(j);
+
+        }
+
+
+    }
+}
